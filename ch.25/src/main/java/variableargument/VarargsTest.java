afb9ffbcb5df769a046c2d02dd1acf4de0372b71
Dongwoo Joo <124649157+DongwooJoo@users.noreply.github.com>
2024-12-12 23:59:47 +0900

    ch.25 variable argument

--- ch.25/src/main/java/variableargument/VarargsTest.java
+++ ch.25/src/main/java/variableargument/VarargsTest.java
@@ -20,3 +20,15 @@ public class VarargsTest {
         }
     }
 }
+
+/*
+가변인자의 메소드 정의 방식
+public static void showAll(String...vargs) {...} // A 정의
+public static void showAll(String[] vargs) {...} // B 정의
+A는 B 메소드로 수정하여 컴파일을 완료한다.
+
+showAll("Box", "Toy", "Apple"); // A 메소드 사용방식
+showAll(new String[]{"Box", "Toy", "Apple"}); // B 메소드 사용방식
+
+A는 B 메소드 사용방식으로 수정하여 컴파일을 완료한다.
+*/
